0f27d76f5ba92dbf1fb00e2c92632bfa3f1a3933
yilip <yi_lip@163.com>
2016-02-18 15:50:30 +0800

    解决冲突

--- src/main/java/com/lip/hello/controller/HelloController.java
+++ src/main/java/com/lip/hello/controller/HelloController.java
@@ -3,7 +3,6 @@ package com.lip.hello.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -11,28 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.lip.hello.core.model.User;
-import com.lip.hello.core.service.IUserService;
 import com.lip.hello.service.ITestService;
 
 @Controller
 public class HelloController {
-	@Resource
-<<<<<<< HEAD
-=======
-	private IUserService userService;
-	@Resource
->>>>>>> 198a5971c26eaf21e5ba58cf808d705e9f69bb8c
 	private ITestService testService;
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public @ResponseBody Map<String, Object> sayHello(HttpServletRequest request) {
 		Map<String, Object> resultMap = new HashMap<>();
-<<<<<<< HEAD
-=======
-		User user=userService.getUser("10000216");
-		resultMap.put("name", user.getAgentId());
->>>>>>> 198a5971c26eaf21e5ba58cf808d705e9f69bb8c
 		resultMap.put("name", testService.getName());
 		resultMap.put("msg", true);
 		return resultMap;
