41571e56237d980895d2da1e6b70234d65ea1c90
yilip <yi_lip@163.com>
2016-07-01 10:04:29 +0800

    maven部署配置

--- src/test/java/com/lip/testmybatis/TestMyBatis.java
+++ src/test/java/com/lip/testmybatis/TestMyBatis.java
@@ -8,8 +8,6 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.lip.hello.core.model.User;
-import com.lip.hello.core.service.IUserService;
 import com.lip.hello.service.ITestService;
 
 @RunWith(SpringJUnit4ClassRunner.class) // 表示继承了SpringJUnit4ClassRunner类
@@ -17,8 +15,6 @@ import com.lip.hello.service.ITestService;
 
 public class TestMyBatis {
 	private static Logger logger = Logger.getLogger(TestMyBatis.class);
-	@Resource
-	private IUserService userService;
 	@Resource
 	private ITestService testService;
 	// private ApplicationContext ac = null;
@@ -32,8 +28,6 @@ public class TestMyBatis {
 	@Test
 	public void test1() {
 		// System.out.println(user.getUserName());
-		User user = userService.getUser("10000216");
-		logger.warn("name:"+user.getAgentId());
 		logger.info("值：" + testService.getName());
 	}
 }
